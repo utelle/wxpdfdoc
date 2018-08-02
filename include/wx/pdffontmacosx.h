9891f0bb1d1f64cda4ec4d3d6a1ebecc0399633b
utelle <ulrich.telle@gmx.de>
2018-08-02 23:22:04 +0200

    Solve issue #44
    In the wxMAC build wxPdfDocument detects which font system is available by checking for the symbols wxOSX_USE_CORE_TEXT and wxOSX_USE_ATSU_TEXT. Since wxWidgets 3.x these symbols are no longer defined, although the CoreText font system now seems to be the default. For that reason wxPdfDocument now assumes for wxWidgets 3.x that CoreText is available and uses related methods to identify fonts which are represented by a wxFont object.

--- include/wx/pdffontmacosx.h
+++ include/wx/pdffontmacosx.h
@@ -17,8 +17,11 @@
 
 #if defined(__WXMAC__)
 
-#if wxCHECK_VERSION(2,9,0)
-  // wxWidgets 2.9.x or higher
+#if wxCHECK_VERSION(3,0,0)    // wxWidgets 3.0.0 or higher
+  // Using CoreText fonts is now the default for wxWidgets 3.x
+  #define wxPDFMACOSX_HAS_CORE_TEXT 1
+  #define wxPDFMACOSX_HAS_ATSU_TEXT 0
+#elif wxCHECK_VERSION(2,9,0)  // wxWidgets 2.9.x or higher
   #include <wx/osx/private.h>
   #define wxPDFMACOSX_HAS_CORE_TEXT wxOSX_USE_CORE_TEXT
   #define wxPDFMACOSX_HAS_ATSU_TEXT wxOSX_USE_ATSU_TEXT
