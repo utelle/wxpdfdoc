ef625cb914fafe1236a75c5b4a93c2e11a1b885f
utelle <ulrich.telle@gmx.de>
2023-11-16 16:46:40 +0100

    Prepare release of wxPdfDocument 1.0.3

--- docs/html/menudata.js
+++ docs/html/menudata.js
@@ -95,7 +95,7 @@ var menudata={children:[
 {text:"x",url:"functions_vars.html#index_x"}]},
 {text:"Enumerations",url:"functions_enum.html"},
 {text:"Enumerator",url:"functions_eval.html"},
-{text:"Related Functions",url:"functions_rela.html"}]}]},
+{text:"Related Symbols",url:"functions_rela.html"}]}]},
 {text:"Files",url:"files.html",children:[
 {text:"File List",url:"files.html"},
 {text:"File Members",url:"globals.html",children:[
